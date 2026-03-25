1678939fd7957aa599358e5feb5e334e3f892b37
Jean Paul Andrade Delgadillo <jean-paul-a@hotmail.com>
2026-03-25 17:15:46 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,4 +8,4 @@ def test_lambda_handler():
     assert response["statusCode"] == 200
 
     body = json.loads(response["body"])
-    assert body["message"] == "Hello World"
+    assert body["message"] == "Hello Mundo"
